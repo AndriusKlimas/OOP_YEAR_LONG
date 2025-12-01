fb8c769ff72be1eb6564d74bed032d35a4c01a78
Noah <DOO279210@student.dkit.ie>
2025-12-01 13:52:45 +0000

    add PlayRecord class for managing play history and update User class to use a dictionary for play records

--- user_records.py
+++ user_records.py
@@ -1,5 +1,23 @@
+from __future__ import annotations
+
+class PlayRecord:
+
+    record_counter: int = 1
+
+    def __init__(self, username: str, video_id: int, position_in_seconds: int):
+        self._play_id = PlayRecord.record_counter
+        self._username = username
+        self._video_id = video_id
+        self._position_in_seconds = position_in_seconds
+
+        PlayRecord.record_counter += 1
+
+    def get_play_id(self):
+        return self._play_id
+
+
 class User:
-    _play_history = []
+    _play_history = {}
 
     def __init__(self, username: str, password: str):
         self._username = username
@@ -21,30 +39,26 @@ class User:
             case "long":
                 return f"Username: {self.get_username()} Password: ********"
             case _:
-                return "Please enter a valid format"
+                print("Invalid option selected")
+                return repr(self)
 
-    def __eq__(self, other: "User"):
-        if not other:
-            return False
+    def __eq__(self, other: object) -> bool | NotImplemented:
+        if not isinstance(other, User):
+            return NotImplemented
+
         return self._username == other._username
 
-    def __ne__(self, other: "User"):
-        if self == other:
-            return False
-        else:
+    def __ne__(self, other: object) -> bool | NotImplemented:
+        if not isinstance(other, User):
             return True
 
-    @staticmethod
-    def get_play_id():
-        play = len(User._play_history) + 1
-        return play
+        return self._username != other._username
 
-    def create_play_record(self):
-        record = 
 
     def start_play(self, video_id: int, pos: int = 0):
-        if video_id is None or pos is None:
+        if video_id <= 0 or pos < 0:
             return False
         else:
-            self._play_history.append()
+            play_record = PlayRecord(PlayRecord.record_counter, self._username, video_id, pos)
+            self._play_history[play_record.get_play_id()] = play_record
             return True
