39b4db3bb5dcf19ea292b58430f846075599fb72
Noah <DOO279210@student.dkit.ie>
2025-12-01 11:07:51 +0000

    add additional methods to User class for string representation and comparison

--- user_records.py
+++ user_records.py
@@ -9,3 +9,29 @@ class User:
         return self._username
 
     def __str__(self):
+        return f"Username: {self.get_username()} Password = ********"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}\nUsername: {self.get_username()}\nPassword: ********"
+
+    def __format__(self, format_spec: str):
+        match format_spec:
+            case "short":
+                return f"Username: {self.get_username()}"
+            case "long":
+                return f"Username: {self.get_username()} Password: ********"
+            case _:
+                return "Please enter a valid format"
+
+    def __eq__(self, other: "User"):
+        if not other:
+            return False
+        return self._username == other._username
+
+    def __ne__(self, other: "User"):
+        if self == other:
+            return False
+        else:
+            return True
+
+    def start_play(self, video_id: int):
