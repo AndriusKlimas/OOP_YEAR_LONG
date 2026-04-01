00dd143243ea772c3cf3f21f9994c0e084512e02
Andrius Klimas <andrius.klimas01@gmail.com>
2026-04-01 17:59:17 +0100

    code: updated entire file, please look at andrius_dev for more info on changes

--- catalogue.py
+++ catalogue.py
@@ -1,152 +1,279 @@
 #imports annotations for type hinting
 
 #**FOR ANDRIUS - FMT2, V1, V3 in playrecord and search feature for app**
+#imports annotations for type hinting
 from __future__ import annotations
 
 from types import NotImplementedType
+from typing import List, Optional
 
 
 #Creating the class
 class Video:
     #Created a class variable to stare all valid genres
-    VALID_GENRES = ["action", "comedy", "drama", "horror", "romance", "scifi", "documentary", "animation", "thriller", "crime"]
+    _VALID_GENRES = ["action", "comedy", "drama", "horror", "romance", "scifi", "documentary", "animation", "thriller", "crime"]
 
     #creating a constructor
-    def __init__(self, _video_id : int, title:str, description:str, _duration_seconds:int, _release_year:int, _genres:list = []):
-        self._genres = _genres
-        self._video_id = _video_id
-        self.title = title
-        self.description = description
-        self._duration_seconds = _duration_seconds
-        self._release_year = _release_year
+    def __init__(
+        self,video_id: int,title: str,description: str,duration_seconds: int,release_year: int,genres: Optional[List[str]] = None) -> None:
+        """Initialize a Video object.
+
+        Args:
+            video_id: Unique identifier for the video.
+            title: Title of the video.
+            description: Description of the video.
+            duration_seconds: Duration of the video in seconds.
+            release_year: Release year of the video.
+            genres: Optional list of genres; defaults to an empty list.
+        """
+        self._genres: List[str] = list(genres) if genres is not None else []
+
+        if not Video.validate_id(video_id):
+            print("Invalid video id.")
+            exit()
+        self._video_id: int = video_id
+
+        if not Video.validate_title(title):
+            print("Invalid title.")
+            exit()
+        self.title: str = title
+
+        if not Video.validate_description(description):
+            print("Invalid description.")
+            exit()
+        self.description: str = description
+
+        if not Video.validate_duration_seconds(duration_seconds):
+            print("Invalid duration seconds.")
+            exit()
+        self._duration_seconds: int = duration_seconds
+
+        if not Video.validate_release_year(release_year):
+            print("Invalid release year.")
+            exit()
+        self._release_year: int = release_year
 
     #method to get title
     def get_title(self) -> str:
-        """ returning a private class variable
+        """Return the title of the video.
 
-        return: (str) title of the video
+        Returns:
+            The title of the video.
         """
         return self.title
 
     #Creating a static method
     @staticmethod
-    #setting a method called return_valid_genres
-    def return_valid_genres() -> list:
-        """ returning all valid genres in a duplicate list
+    def return_valid_genres() -> List[str]:
+        """Return a list of all valid genres.
 
-        return: (list) list of all valid genres
+        Returns:
+            A copy of the list of all valid genres.
         """
-        #returning a copy list of all valid genres
-        return list(Video.VALID_GENRES)
+        return list(Video._VALID_GENRES)
 
     #method to add genre
-    def add_genre(self, genre:str) -> bool:
+    def add_genre(self, genre: str) -> bool:
+        """Add a genre to the video if it is valid and not already present.
+
+        Args:
+            genre: The genre to be added.
+
+        Returns:
+            True if the genre was successfully added, False otherwise.
         """
-        args:
-            genre (str): genre to be added
-        return:
-            bool: True if genre was added, False otherwise
-        """
-        #checking if the genre is not in the video, and if it is in the valid genres
-        if genre not in self._genres and genre in Video.VALID_GENRES:
-            #adding it to the video if it is not in the list and in valid genres
+        if genre not in self._genres and genre in Video.validate_genre(genre):
             self._genres.append(genre.lower())
-            #returning TRUE
             return True
-        #else
         else:
-            #returning FALSE
             return False
 
     #method to check genre
-    def check_genre(self, genre:str) -> bool:
-        """ checking if a genre is in the video and a valid genre
-        args:
-            genre (str): genre to be checked
-        return:
-            bool: True if genre is in the video, False otherwise
+    def check_genre(self, genre: str) -> bool:
+        """Check if a genre is present in the video.
 
+        Args:
+            genre: The genre to be checked.
+
+        Returns:
+            True if the genre is in the video, False otherwise.
         """
-        #checking if the genre is in the video and if it is in the valid genres
-        if genre.lower() in self._genres and genre.lower() in Video.VALID_GENRES:
-            #returning True
+        if genre.lower() in self._genres and genre.lower() in Video.validate_genre(genre):
             return True
-        #else
         else:
-            #Returning false
             return False
+
+
+    #New thing added
+    @staticmethod
+    def validate_genre(genre: str) -> bool:
+        if genre.lower() in Video._VALID_GENRES:
+            return True
+        else:
+            return False
+
 
     #for good practice to return private variables
     def get_video_id(self) -> int:
-        """ getting the private class variable video id
+        """Get the unique identifier of the video.
 
-        return:
-            returns the video id (int)
+        Returns:
+            The video ID.
         """
         return self._video_id
 
     # for good practice to return private variables
     def get_duration_seconds(self) -> int:
-        """ geting the private class variable duration seconds
+        """Get the duration of the video in seconds.
 
-        return:
-            returns the duration in seconds (int)
+        Returns:
+            The duration in seconds.
         """
         return self._duration_seconds
 
     # for good practice to return private variables
     def get_release_year(self) -> int:
-        """ getting the private class variable release year
+        """Get the release year of the video.
 
-        return:
-            returns the release year (int)
+        Returns:
+            The release year.
         """
         return self._release_year
 
     # for good practice to return private variables
-    def get_genres(self) -> list:
-        """ getting the private class variable genres
+    def get_genres(self) -> List[str]:
+        """Get the list of genres for the video.
 
-        return:
-            returns the genres (list)
+        Returns:
+            A copy of the genres list.
         """
         return list(self._genres)
 
+    @staticmethod
+    def validate_id(video_id: int) -> bool:
+        """Validate if a video ID is valid.
+
+        A valid video ID must be a non-negative integer.
+
+        Args:
+            video_id: The video ID to validate.
+
+        Returns:
+            True if the video ID is valid, False otherwise.
+        """
+        if video_id is None:
+            return False
+        if video_id < 0:
+            return False
+        if not isinstance(video_id, int):
+            return False
+        return True
+
+    @staticmethod
+    def validate_title(title: str) -> bool:
+        """Validate if a title is valid.
+
+        A valid title must not be None.
+
+        Args:
+            title: The title to validate.
+
+        Returns:
+            True if the title is valid, False otherwise.
+        """
+        if title is None:
+            return False
+        return True
+
+    @staticmethod
+    def validate_description(description: str) -> bool:
+        """Validate if a description is valid.
+
+        A valid description must not be None.
+
+        Args:
+            description: The description to validate.
+
+        Returns:
+            True if the description is valid, False otherwise.
+        """
+        if description is None:
+            return False
+        return True
+
+    @staticmethod
+    def validate_duration_seconds(duration_seconds: int) -> bool:
+        """Validate if a duration in seconds is valid.
+
+        A valid duration must be a non-negative integer.
+
+        Args:
+            duration_seconds: The duration in seconds to validate.
+
+        Returns:
+            True if the duration is valid, False otherwise.
+        """
+        if duration_seconds is None:
+            return False
+        if duration_seconds < 0:
+            return False
+        return True
+
+    @staticmethod
+    def validate_release_year(release_year: int) -> bool:
+        """Validate if a release year is valid.
+
+        A valid release year must be a non-negative integer.
+
+        Args:
+            release_year: The release year to validate.
+
+        Returns:
+            True if the release year is valid, False otherwise.
+        """
+        if release_year is None:
+            return False
+        if release_year < 0:
+            return False
+        return True
+
+
     #to print out the info about the video
     def __str__(self) -> str:
-        """ to print out info for user
+        """Return a user-friendly string representation of the video.
 
-        return:
-            returns the string representation of the video
+        Returns:
+            A formatted string with video information.
         """
         return f"Title is {self.title} and description is {self.description}, Duration is {self.get_duration_seconds()} seconds, Release year is {self.get_release_year()}. Genres are {', '.join(self.get_genres())}"
 
     #for a developer
     def __repr__(self) -> str:
-        """ to print out info for developer
+        """Return a developer-friendly string representation of the video.
 
-        return:
-            returns the developer representation of the video
+        Returns:
+            A formatted string with all video details and ID.
         """
         return f"Video_ID: {self.get_video_id()}, Title: {self.title}, Description: {self.description}, Duration: {self.get_duration_seconds()}, Release Year: {self.get_release_year()}, Genres: {self.get_genres()}"
 
     #defining equality method
     def __hash__(self) -> int:
-        """ getting the hash value of the video
+        """Get the hash value of the video.
 
-        return:
-            returns the hash value of the video
+        Returns:
+            The hash value based on the video ID.
         """
         return hash((self.get_video_id()))
 
     #defining format method
     def __format__(self, format_spec: str) -> str:
-        """ formatting the video info
-        args:
-            format_spec (str): format specification
+        """Format the video information based on the format specification.
 
-        return:
-            returns the formatted string representation of the video
+        Args:
+            format_spec: The format specification ("short" or "long").
+
+        Returns:
+            A formatted string representation of the video.
         """
         match format_spec:
             case "short":
@@ -157,12 +284,14 @@ class Video:
                 return "Please enter a valid format"
 
     def __eq__(self, other: object) -> bool | NotImplementedType:
-        """ checking equality between two videos
-        args:
-            other (object): other object to compare with
-        return:
-            bool: True if equal, False otherwise
+        """Check equality between two videos based on their video IDs.
 
+        Args:
+            other: The object to compare with.
+
+        Returns:
+            True if both videos have the same ID, False otherwise.
+            NotImplemented if other is not a Video instance.
         """
         if not isinstance(other, Video):
             return NotImplemented
@@ -170,14 +299,68 @@ class Video:
         return self._video_id == other._video_id
 
     def __ne__(self, other: object) -> bool | NotImplementedType:
+        """Check inequality between two videos based on their video IDs.
 
-        """ checking inequality between two videos
-        args:
-            other (object): other object to compare with
-        return:
-            bool: True if not equal, False otherwise
+        Args:
+            other: The object to compare with.
+
+        Returns:
+            True if the videos have different IDs, False otherwise.
+            NotImplemented if other is not a Video instance.
         """
         if not isinstance(other, Video):
-            return True
+            return NotImplemented
 
         return self._video_id != other._video_id
+
+    def __le__(self, other: object) -> bool | NotImplementedType:
+        """Check equality between two videos based on their video IDs.
+        Args:
+            other: The object to compare with.
+        Returns:
+        True if both videos have the same ID, False otherwise.
+        NotImplemented if other is not a Video instance.
+        """
+        if not isinstance(other, Video):
+            return NotImplemented
+        return self._video_id <= other._video_id
+
+    def __ge__(self, other: object) -> bool | NotImplementedType:
+        """Check inequality between two videos based on their video IDs.
+        Args:
+            other: The object to compare with.
+
+        return:
+        True if both videos have the same ID, False otherwise.
+        NotImplemented if other is not a Video instance.
+            """
+        if not isinstance(other, Video):
+            return NotImplemented
+        return self._video_id >= other._video_id
+
+    def __lt__(self, other: object) -> bool | NotImplementedType:
+        """Check equality between two videos based on their video IDs.
+        Args:
+            other: The object to compare with.
+
+        return:
+        True if both videos have the same ID, False otherwise.
+        NotImplemented if other is not a Video instance.
+            """
+        if not isinstance(other, Video):
+            return NotImplemented
+        return self._video_id < other._video_id
+
+
+    def __gt__(self, other: object) -> bool | NotImplementedType:
+        """Check inequality between two videos based on their video IDs.
+        Args:
+            other: The object to compare with.
+
+        return:
+        True if both videos have the same ID, False otherwise.
+        NotImplemented if other is not a Video instance.
+            """
+        if not isinstance(other, Video):
+            return NotImplemented
+        return self._video_id > other._video_id
