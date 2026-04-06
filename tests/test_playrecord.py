7428ca872176881c8f332f3d71759928b19140ff
Noah <noahedclarke@gmail.com>
2026-04-06 13:16:45 +0100

    feat: add tests for PlayRecord object retrieval in test_playrecord.py

--- tests/test_playrecord.py
+++ tests/test_playrecord.py
@@ -11,3 +11,24 @@ class TestPlayRecordCreation:
         assert playrecord.get_video_id() == 10
         assert playrecord.get_pos() == 25
         assert playrecord.get_play_id() > 0
+
+class TestPlayRecordRetrieval:
+    """Test play record object retrieval"""
+
+    def test_get_username(self):
+        playrecord = PlayRecord("NoahClarke123", 10, 25)
+
+        assert playrecord.get_username() == "NoahClarke123"
+        assert playrecord.get_username() != "NoahClarke234"
+
+    def test_get_video_id(self):
+        playrecord = PlayRecord("NoahClarke123", 10, 25)
+
+        assert playrecord.get_video_id() == 10
+        assert playrecord.get_video_id() != 11
+
+    def test_get_pos(self):
+        playrecord = PlayRecord("NoahClarke123", 10, 25)
+
+        assert playrecord.get_pos() == 25
+        assert playrecord.get_pos() != 30
