bfb08987e0894692d9f7e90099e08429ab195896
Noah <noahedclarke@gmail.com>
2026-04-06 11:36:35 +0100

    feat: add tests for user retrieval methods in test_user.py

--- tests/test_user.py
+++ tests/test_user.py
@@ -9,7 +9,17 @@ class TestUserCreation:
         assert user.get_username() == "NoahClarke123"
         assert user.get_password() == "Password123!"
 
+class TestUserRetrieval:
+    """Test user object retrieval"""
+    def test_get_username(self):
+        user = User("NoahClarke123", "Password123!")
+        assert user.get_username() == "NoahClarke123"
+        assert user.get_username() != "NoahClarke234"
 
+    def test_get_password(self):
+        user = User("NoahClarke123", "Password123!")
+        assert user.get_password() == "Password123!"
+        assert user.get_password() != "Password"
 
 
 
