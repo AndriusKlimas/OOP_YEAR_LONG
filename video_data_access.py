aebf64e6630b3da407eb3742b690f9f93b2f7a34
Andrius Klimas <andrius.klimas01@gmail.com>
2026-05-05 16:15:01 +0100

    feat: set up init for video data

--- video_data_access.py
+++ video_data_access.py
@@ -9,3 +9,19 @@ logger = logging.getLogger(__name__)
 
 
 class JSONVideoDataAccess():
+    def __init__(self, filename: str):
+        """Initialises JSON data access
+
+        Args:
+            filename(str): Path to JSON file
+
+        Raises:
+            FileNotFoundError: If video data file not exists
+        """
+        try:
+            with open(filename, "r") as file:
+                pass
+        except FileNotFoundError:
+            raise FileNotFoundError(f"File not found: {filename}")
+
+        self._filename = filename
