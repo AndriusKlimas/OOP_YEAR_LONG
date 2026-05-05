bef0b3b369823bfe1ce48721e4a6927cd6ad1a8b
Andrius Klimas <andrius.klimas01@gmail.com>
2026-05-05 16:21:12 +0100

    feat: set up store for video data

--- video_data_access.py
+++ video_data_access.py
@@ -56,3 +56,21 @@ class JSONVideoDataAccess():
         except Exception as e:
             logger.error(f"Error loading videos from {self._filename}: {e}")
             raise
+
+    def store(self, videos: dict[str, list[Video]]) -> None:
+        """Store videos to JSON file
+
+        Args:
+            videos (dict[str, list[Video]]): Dict with titles as keys and lists of Video objects as values
+        """
+        try:
+            videos_data = {}
+            for title, videos_list in videos.items():
+                videos_data[title] = [video.to_dict() for video in videos_list]
+
+            with open(self._filename, "w") as file:
+                json.dump(videos_data, file, indent=2)
+            logger.info(f"Successfully saved videos to file {self._filename}")
+        except Exception as e:
+            logger.error(f"Error saving videos to {self._filename}: {e}")
+            raise
